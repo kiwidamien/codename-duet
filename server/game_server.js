e297b63d229d2808ba3203ca7cbd558f1a26d047
kiwidamien <djmartin@ucdavis.edu>
2019-12-09 00:40:59 -0800

    duplicated code (wawa)

--- server/game_server.js
+++ server/game_server.js
@@ -1,6 +1,10 @@
+//import {STATUS, DECK, REASON, PHASE} from './constants.js';
+const CONSTANTS = require('./constants.js');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+
+console.log(CONSTANTS);
 
 server.listen(2000);
 console.log("Server started");
@@ -12,6 +16,7 @@ var IO = require('socket.io')(server, {});
 
 IO.sockets.on('connection', (socket) => {
   socket.id = SOCKET_LIST.length;
+  socket.room = 42;
   socket.message = "hello";
   socket.active = true;
 
@@ -22,4 +27,11 @@ IO.sockets.on('connection', (socket) => {
   socket.on('click_card', () => {});
   socket.on('pass', () => {});
   socket.on('give-clue', () => {});
+
+  socket.on('subscribeToTimer', (interval) => {
+    console.log(`Client is subscribing to timer to update every ${interval} milliseconds`);
+    setInterval( () => {
+      socket.emit('timer', new Date());
+    }, interval);
+  });
 });
