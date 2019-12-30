6c3d5f29fbb3f1e71d38b0033d1bf78795b60021
kiwidamien <djmartin@ucdavis.edu>
2019-12-29 18:05:12 -0800

    added ability to pick words;

--- src/lobby/NewGameForm.js
+++ src/lobby/NewGameForm.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
 import {Link} from 'react-router-dom';
+import drawRandomWords from './words.js';
+import './new_game.css';
+
 
 const makeNewGame = async (event, name, setNewGameURLs) => {
   event.preventDefault();
@@ -10,9 +13,20 @@ const makeNewGame = async (event, name, setNewGameURLs) => {
   .then(games => {console.log(games); setNewGameURLs(games); });
 }
 
+const getDefaultWords = () => {
+  return ['apple', 'mandarin', 'persimmon', 'javascript', 'kiwi',
+          'love', 'jedi', 'clone', 'danish', 'tiramasu',
+          'landscape', 'wildlife', 'hike', 'embassy', 'parasite',
+          'elephant', 'travel', 'siblings', 'testing', 'example',
+          'teacher', 'astronaut', 'bootcamp', 'bake', 'meditate'
+        ];
+}
+
 const NewGameForm = () => {
   const [name, setName] = useState('');
   const [newGameURLs, setNewGameURLs] = useState(['', '']);
+  const [words, setWords] = useState(getDefaultWords());
+
   return (
     <div>
     <h2>New Game</h2>
@@ -24,8 +38,29 @@ const NewGameForm = () => {
       value={name}
       onChange={e => setName(e.target.value)}
     />
-    <Link to="/">Return home</Link>
-    <button type="submit">Create Game</button>
+    <div class="word-selection-container">
+      <div class="new-word-grid">
+        {words.map( (word, index) => <input
+            value={word}
+            key={index}
+            onChange={(e) => {
+              const newWords = [...words];
+              newWords[index] = e.target.value;
+              setWords(newWords);
+            }}
+            />)}
+      </div>
+
+      <div class="word-grid-controls">
+        <button onClick={() => setWords(drawRandomWords())}>Random Draw</button>
+        <button onClick={() => setWords(getDefaultWords())}>Default</button>
+      </div>
+
+      <div class="word-grid-controls">
+        <Link to="/" className="cancel-button">Return home</Link>
+        <button type="submit" className="submit-button">Create Game</button>
+      </div>
+    </div>
   </form>
   <Modal isOpen={newGameURLs[0]}>
   <ul>
@@ -38,3 +73,66 @@ const NewGameForm = () => {
 }
 
 export default NewGameForm;
+
+/*
+<html>
+<link href='mock_new_game.css' rel='stylesheet' />
+<body>
+
+
+<div>
+<h2>New Game</h2>
+<form class="new-game">
+<label htmlFor="name">Name</label>
+<input
+  type="text"
+  id="name"
+  value={name}
+/>
+<div class="new-word-selection">
+  <h3>Words to use</h3>
+  <div class="word-selection-container">
+    <div class="new-word-grid">
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+    </div>
+    <div class="word-grid-controls">
+      <button>Random Draw</button>
+      <button>Default</button>
+    </div>
+    </div>
+</div>
+
+<div class="word-grid-controls">
+  <a href="/" class="cancel-button">Return to lobby</a>
+  <button type="submit" class="submit-button">Create Game</button>
+</div>
+</form>
+</div>
+
+</body>
+</html>
+*/
